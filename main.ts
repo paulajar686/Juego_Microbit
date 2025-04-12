003f258d55a9cd09f31ee62c1ddb4f0c42daa2ce
paulajar686 <103377534+paulajar686@users.noreply.github.com>
2025-04-12 21:13:10 +0200

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,31 @@
+input.onButtonPressed(Button.A, function () {
+    jugador.change(LedSpriteProperty.X, -1)
+})
+input.onButtonPressed(Button.B, function () {
+    jugador.change(LedSpriteProperty.X, 1)
+})
+let objeto: game.LedSprite = null
+let jugador: game.LedSprite = null
+jugador = game.createSprite(2, 4)
+basic.forever(function () {
+    basic.pause(randint(1000, 1500))
+    objeto = game.createSprite(randint(0, 4), 0)
+    if (game.score() < 15) {
+        for (let index = 0; index < 4; index++) {
+            basic.pause(500)
+            objeto.change(LedSpriteProperty.Y, 1)
+        }
+    }
+    if (game.score() >= 15) {
+        for (let index = 0; index < 4; index++) {
+            basic.pause(200)
+            objeto.change(LedSpriteProperty.Y, 1)
+        }
+    }
+    if (objeto.isTouching(jugador)) {
+        game.gameOver()
+    }
+    basic.pause(100)
+    game.addScore(1)
+    objeto.delete()
+})
